5eaa3d8cae762cf607043943f5426374bd199c9f
Marcus "Loki" Jenz <jenz@silpion.de>
2018-07-03 21:40:32 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DicerComponent } from './dicer/dicer.component';
+import {HttpClientModule} from '@angular/common/http';
+import {RolzApiService} from './shared/service/rolz-api.service';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DicerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+//    HttpClientInMemoryWebApiModule.forRoot(
+//      InMemoryDataService, {dataEncapsulation: false}
+//    )
   ],
-  providers: [],
+  providers: [
+    RolzApiService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/model/dice-result.ts
@@ -0,0 +1,7 @@
+export class DiceResult {
+  result: number;
+  details: string;
+  code: string;
+  illustration: string;
+  timestamp: number;
+}
